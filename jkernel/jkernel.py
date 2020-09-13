1a4cd46114e9061f825ea28634f09fafbb268cbe
Jeremy Howard <j@fast.ai>
2020-09-13 04:49:54 -0700

    refactor

--- jkernel/jkernel.py
+++ jkernel/jkernel.py
@@ -1,56 +1,14 @@
-###############################################################################
-#
-# File:        jkernel.py
-# Author:      Martin Saurer, 19.02.2016
-# Description: Jupyter / J - integration.
-#
-# License:     GPL Version 3 (see gpl3.txt)
-#
-# Note 1:      To enforce python run in 32bit mode on Mac OS X,
-#              set the following environment variable:
-#              export VERSIONER_PYTHON_PREFER_32_BIT=yes
-#
-###############################################################################
-
-###############################################################################
-# Import modules
-###############################################################################
-
-# Imports
-import os
-import sys
-import base64
-
-# Import Jupyter Kernel
+import os,sys,base64
+from pathlib import Path
 from ipykernel.kernelbase import Kernel
-
-# Append this files path to sys.path
-sys.path.append(os.path.dirname(__file__))
-
-# Import J interface
-import jinter
-
-###############################################################################
-# Global variables
-###############################################################################
+from .import jinter
 
 canvasnum = 1
 
-###############################################################################
-# Jupyter Kernel Class
-###############################################################################
-
-# Jupyter Kernel Class
 class JKernel(Kernel):
-
-    # Basic kernel setup
     implementation         = 'jkernel'
     implementation_version = '3.2.0'
-    language_info          = {
-        'name'            : 'J',
-        'mimetype'        : 'text/J',
-        'file_extension'  : 'ijs'
-    }
+    language_info          = { 'name': 'J', 'mimetype': 'text/J', 'file_extension'  : 'ijs' }
     banner                 = 'J Programming Language - Jupyter Kernel'
     help_links             = [
         {
@@ -63,16 +21,11 @@ class JKernel(Kernel):
         }
     ]
 
-    # Constructor
     def __init__(self, *args, **kwargs):
-        # Call super class constructor
         super(JKernel,self).__init__(*args,**kwargs)
 
-        # Get required OS environment variables
-        JInsFol = os.getenv('J_INSTALLATION_FOLDER')
-        JBinFol = os.getenv('J_BIN_FOLDER')
-
-        # Initialize J
+        JInsFol = os.getenv('J_INSTALLATION_FOLDER', f'{os.getenv("CONDA_PREFIX")}/share/j901')
+        JBinFol = os.getenv('J_BIN_FOLDER', f'{JInsFol}/bin')
         self.J = jinter.J(JInsFol,JBinFol)
 
         # Initialize J JHS
@@ -81,195 +34,86 @@ class JKernel(Kernel):
         self.J.Exec('load \'jhs\'')
 
         # This one is not defined on Windows ???
-        if os.name == 'nt':
-            self.J.Exec('iad_pplatimg_ =: 15!:14@boxopen')
-        # end if
+        if os.name == 'nt': self.J.Exec('iad_pplatimg_ =: 15!:14@boxopen')
 
         # Get J user folder
         self.J.Exec('tmpstr_jinter_ =: jpath \'~user\'')
-        s = self.J.GetStrVar('tmpstr_jinter_')
-        if os.name == 'nt':
-            s = s.replace('/','\\')
-        else:
-            s = s.replace('\\','/')
-        # end if
-        self.J.JUsrFol = s
-    # end def
+        self.J.JUsrFol = Path(self.J.GetStrVar('tmpstr_jinter_'))
 
-    # Override do_execute method
-    def do_execute(self,
-                   code,
-                   silent,
-                   store_history=True,
-                   user_expressions=None,
-                   allow_stdin=False):
-
-        # Global variables
+    def _retr(self,output, silent):
         global canvasnum
+        if silent: return
+        content = { 'source': 'J', 'metadata': {}, 'data': {} }
+        if output.startswith('<!-- j html output a -->'):
+            try:
+                prefix = '<!-- j html output a --><img'
+                if output.startswith(prefix):
+                    # Extract image name
+                    stapos = output.find('src=')
+                    imgnam = output[stapos+11:]
+                    imgnam = imgnam[:imgnam.find('?')]
+                    imgnam = self.J.JUsrFol/'temp'/imgnam
 
-        # Debug output
-        #self.log.error('*** do_execute')
+                    # Read image in base64 encoded data
+                    with open(imgnam,'rb') as fp: imgdat = base64.b64encode(fp.read()).decode()
+                    imgnam.unlink(missing_ok=False)
 
-        # Initialization
-        output = ''
+                    # Send output data
+                    content['data']['image/png'] = imgdat
+                    #self.log.error(repr(content))
+                    self.send_response(self.iopub_socket, 'display_data', content)
 
-        # Split cell into single lines
-        lines = code.splitlines()
+                # Other output (plot)
+                else:
+                    htmnam = self.J.JUsrFol/'temp'/'plot.html'
+
+                    # Read html file
+                    with open(htmnam,'r') as fp:
+                        htmdat = fp.read() \
+                                .replace('</article>', '</article><script>graph();</script>') \
+                                .replace('canvas1', f'canvas{canvasnum}')
+                    htmnam.unlink(missing_ok=False)
+
+                    canvasnum += 1
+                    content['data']['text/html'] = htmdat
+                    self.send_response(self.iopub_socket, 'display_data', content)
+
+            except Exception as e:
+                content = { 'name': 'stdout', 'text': f'JKernel: Internal Error.\n{e}\n' }
+                self.send_response(self.iopub_socket,'stream',content)
+
+        # Is it an html output
+        elif output.startswith('<html>'):
+            content['data']['text/html'] = output
+            self.send_response(self.iopub_socket, 'display_data', content)
+        else:
+            # Normal text output
+            content = { 'name': 'stdout', 'text': output }
+            self.send_response(self.iopub_socket,'stream',content)
+
+    def do_execute(self, code, silent, *args, **kwargs):
+        global canvasnum
 
         # Pass input to J (new version)
         # Multi-line statements (like verb definitions) are now possible
         # Only the output of the last line (statement) is printed
-        lines = [line for line in lines if line.strip() != '']
+        lines = [line for line in code.splitlines() if line.strip()]
         lastline = ''
-        if len(lines) > 0:
-            # Check last line (end of multiline statement)
-            if not lines[-1].strip() == ')':
-                lastline = lines[-1]
-                del lines[-1]
-                code = '\n'.join(lines)
-            # end if
-        # end if
+        # Check last line (end of multiline statement)
+        if lines:
+            lastline = lines.pop().strip()
+            if not lastline == ')': code = '\n'.join(lines)
 
-        # Prepare code
-        code = code.replace('\'','\'\'')
-
-        # Execute code
-        self.J.Exec('input_jinter_ =: \'' + code + '\'')
+        code = code.replace("'","''")
+        self.J.Exec(f"input_jinter_ =: '{code}'")
         self.J.Exec('0!:110 input_jinter_')
 
         # Process last line (receive output)
-        if lastline.strip() != '':
+        if lastline:
             self.J.Exec(lastline)
             output = self.J.Recv()
-        # end if
+            if output: self._retr(output, silent)
 
-        # Check silent flag
-        if not silent:
-
-            # Debug output
-            #self.log.error('*** not silent')
-
-            # Check output length
-            if len(output) > 0:
-
-                # Debug output
-                #self.log.error('*** len(output) > 0')
-                #self.log.error('*** ' + self.J.JUsrFol)
-
-                # Check output
-                if output.startswith('<!-- j html output a -->'):
-
-                    # Error handler
-                    try:
-
-                        # Is it an image output ?
-                        prefix = '<!-- j html output a --><img'
-                        if output.startswith(prefix):
-
-                            # Extract image name
-                            stapos = output.find('src=')
-                            imgnam = output[stapos+11:]
-                            endpos = imgnam.find('?')
-                            imgnam = imgnam[:endpos]
-                            imgnam = os.path.join(self.J.JUsrFol,
-                                                  'temp',
-                                                  imgnam)
-
-                            # Read image in base64 encoded data
-                            with open(imgnam,'rb') as fp:
-                                imgdat = base64.b64encode(fp.read()).decode()
-                            # end with
-                            try:
-                                os.remove(imgnam)
-                            except:
-                                pass
-                            # end try
-
-                            # Send output data
-                            content = {
-                                'source'   : 'J',
-                                'metadata' : { },
-                                'data'     : {'image/png': imgdat}
-                            }
-                            #self.log.error(repr(content))
-                            self.send_response(self.iopub_socket,
-                                               'display_data',
-                                               content)
-
-                        # Other output (plot)
-                        else:
-
-                            # Generate HTML file name
-                            htmnam = os.path.join(self.J.JUsrFol,
-                                                  'temp',
-                                                  'plot.html')
-
-                            # Read html file
-                            with open(htmnam,'r') as fp:
-                                htmdat = fp.read()
-                                htmdat = htmdat.replace('</article>',
-                                        '</article><script>graph();</script>')
-                                htmdat = htmdat.replace('canvas1',
-                                        'canvas' + str(canvasnum))
-                            # end with
-                            try:
-                                os.remove(htmnam)
-                            except:
-                                pass
-                            # end try
-
-                            # Increment canvasnum
-                            canvasnum += 1
-
-                            content = {
-                                'source'   : 'J',
-                                'metadata' : { },
-                                'data'     : {'text/html': htmdat}
-                            }
-                            self.send_response(self.iopub_socket,
-                                               'display_data',
-                                               content)
-
-                        # end if output.startswith(prefix):
-
-                    except:
-                        # Error output
-                        content = {
-                            'name': 'stdout',
-                            'text': ('JKernel: Internal Error.\n' +
-                                     str(sys.exc_info()[1]) +
-                                     '\n')
-                        }
-                        self.send_response(self.iopub_socket,'stream',content)
-
-                    # end try
-                  
-                # Is it an html output
-                elif output.startswith('<html>'):
-                    content = {
-                        'source':   'J',
-                        'metadata': { },
-                        'data':     {'text/html': output}
-                    }
-                    self.send_response(self.iopub_socket,
-                                       'display_data',
-                                       content)
-
-                else:
-                    # Normal text output
-                    content = {
-                        'name': 'stdout',
-                        'text': output
-                    }
-                    self.send_response(self.iopub_socket,'stream',content)
-
-                # end if output.startswith('<!-- j html output a -->'):
-
-            # end if len(output) > 0:
-
-        # end if not silent:
-
-        # Return
         return {
             'status':           'ok',
             'execution_count':  self.execution_count,
@@ -277,23 +121,10 @@ class JKernel(Kernel):
             'user_expressions': {},
         }
 
-    # end def do_execute()
+    def do_shutdown(self,restart): pass
+    def do_inspect(self,code,cursor_pos,detail_level=0): pass
 
-    # Nothing special here
-    def do_shutdown(self,restart):
-        pass
-
-    def do_inspect(self,code,cursor_pos,detail_level=0):
-        pass
-
-# end class JKernel(Kernel):
-
-# Main entry point
 if __name__ == '__main__':
     from ipykernel.kernelapp import IPKernelApp
     IPKernelApp.launch_instance(kernel_class=JKernel)
-# end if
 
-###############################################################################
-# EOF
-###############################################################################
--- setup.py
+++ setup.py
@@ -1,288 +1,48 @@
-###############################################################################
-#
-# File:        setup.py
-# Author:      Martin Saurer, 01.01.2018
-# Description: Setup script for the J/Jupyter Notebook integration.
-#
-# License:     GPL Version 3 (see gpl3.txt)
-#
-###############################################################################
+import glob,setuptools,os
+from pathlib import Path
+from setuptools import setup
 
-###############################################################################
-# Import modules
-###############################################################################
+DISTNAME = 'j_kernel'
+DESCRIPTION = 'A Jupyter kernel for J.'
+LONG_DESCRIPTION = open('README.md').read()
+MAINTAINER = 'Jeremy Howard'
+MAINTAINER_EMAIL = 'info@howard.fm'
+URL = 'http://github.com/fastai/j_kernel'
+LICENSE = 'GPL'
+REQUIRES = ["jupyter_client (>=4.3.0)", "ipykernel"]
+INSTALL_REQUIRES = REQUIRES
 
-import os
-import sys
-import shutil
+CLASSIFIERS = """\
+Intended Audience :: Science/Research
+License :: OSI Approved :: GPL License
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Topic :: Scientific/Engineering
+Topic :: Software Development
+Topic :: System :: Shells
+"""
 
-###############################################################################
-# Utility functions
-###############################################################################
+import notebook
+nb_dir = Path(notebook.__file__).parent.relative_to(os.environ['CONDA_PREFIX'])
+DATA_FILES = [
+    (f'{nb_dir}/static/components/codemirror/mode/j', ['syntax/J.js']),
+    ('share/jupyter/kernels/j', glob.glob('kernel_definition/*.*')),
+]
 
-# Print exception info
-def print_exception_info():
-    typ,val,trb = sys.exc_info()
-    fil = os.path.split(trb.tb_frame.f_code.co_filename)[1]
-    lin = trb.tb_lineno
-    est = ('Exception: ' + str(typ) + ': ' + str(val) + ' / in ' + str(fil) +
-           ':' + str(lin))
-    print(val)
+setup(
+    name=DISTNAME,
+    version='0.0.1',
+    maintainer=MAINTAINER, maintainer_email=MAINTAINER_EMAIL,
+    packages = setuptools.find_packages(),
+    data_files=DATA_FILES,
+    url=URL, download_url=URL,
+    license=LICENSE,
+    platforms=["Any"],
+    description=DESCRIPTION, long_description=LONG_DESCRIPTION, long_description_content_type='text/x-rst',
+    classifiers=list(filter(None, CLASSIFIERS.split('\n'))),
+    install_requires=INSTALL_REQUIRES
+)
 
-###############################################################################
-# Check pre-requisites
-###############################################################################
-
-print('')
-print('Check pre-requisites ...')
-
-# Get site-packages directory
-site_packages_dir = ''
-for p in sys.path:
-    if p.endswith('site-packages'):
-        site_packages_dir = p
-        break
-
-# Get anaconda/miniconda root directory
-conda_root_dir = os.path.dirname(site_packages_dir)
-conda_root_dir = os.path.dirname(conda_root_dir)
-if os.name != 'nt':
-    conda_root_dir = os.path.dirname(conda_root_dir)
-
-# Build jupyter kernels directory
-jupyter_kernels_dir = os.path.join(conda_root_dir     ,'share'  )
-jupyter_kernels_dir = os.path.join(jupyter_kernels_dir,'jupyter')
-jupyter_kernels_dir = os.path.join(jupyter_kernels_dir,'kernels')
-
-# Build notebook syntax directory
-notebook_syntax_dir = os.path.join(site_packages_dir  ,'notebook'  )
-notebook_syntax_dir = os.path.join(notebook_syntax_dir,'static'    )
-notebook_syntax_dir = os.path.join(notebook_syntax_dir,'components')
-notebook_syntax_dir = os.path.join(notebook_syntax_dir,'codemirror')
-notebook_syntax_dir = os.path.join(notebook_syntax_dir,'mode'      )
-
-# Check accessibility
-ado = True
-if os.access(conda_root_dir,os.W_OK):
-    crd = ' OK.'
-else:
-    crd = ' MISSING or NOT writeable.'
-    ado = False
-if os.access(site_packages_dir,os.W_OK):
-    spd = ' OK.'
-else:
-    spd = ' MISSING or NOT writeable.'
-    ado = False
-if os.access(notebook_syntax_dir,os.W_OK):
-    nsd = ' OK.'
-else:
-    nsd = ' MISSING or NOT writeable.'
-    ado = False
-if os.access(jupyter_kernels_dir,os.W_OK):
-    jkd = ' OK.'
-else:
-    jkd = ' MISSING or NOT writeable.'
-    ado = False
-
-# Print directories
-print('Anaconda/Miniconda root directory ... : ' + conda_root_dir      + crd)
-print('Site Packages directory ............. : ' + site_packages_dir   + spd)
-print('Jupyter Notebook syntax directory ... : ' + notebook_syntax_dir + nsd)
-print('Jupyter Kernels directory ........... : ' + jupyter_kernels_dir + jkd)
-
-# Print check results
-if ado == True:
-    print('ALL destination directories are OK.')
-    print('Done.')
-else:
-    print('ERROR: One or more destination directories are not accessible !!!')
-    sys.exit(0)
-
-###############################################################################
-# Build source files
-###############################################################################
-
-src_root_dir = os.path.dirname(sys.argv[0])
-
-src_kernel_dir   = os.path.join(src_root_dir,'jkernel')
-src_kernel_files = []
-src_kernel_files.append(os.path.join(src_kernel_dir,'__init__.py'))
-src_kernel_files.append(os.path.join(src_kernel_dir,'jinter.py'  ))
-src_kernel_files.append(os.path.join(src_kernel_dir,'jkernel.py' ))
-
-src_kernel_definition_dir   = os.path.join(src_root_dir,'kernel_definition')
-src_kernel_definition_dir   = os.path.join(src_kernel_definition_dir,'jkernel')
-src_kernel_definition_files = []
-src_kernel_definition_files.append(os.path.join(src_kernel_definition_dir,'kernel.json'))
-src_kernel_definition_files.append(os.path.join(src_kernel_definition_dir,'logo-32x32.png'))
-src_kernel_definition_files.append(os.path.join(src_kernel_definition_dir,'logo-64x64.png'))
-
-src_syntax_dir   = os.path.join(src_root_dir,'syntax')
-src_syntax_dir   = os.path.join(src_syntax_dir,'J')
-src_syntax_files = []
-src_syntax_files.append(os.path.join(src_syntax_dir,'J.js'))
-
-###############################################################################
-# Parse arguments and invoke action
-###############################################################################
-
-parm = ''
-if len(sys.argv) > 1: parm = sys.argv[1].lower().strip()
-mode = ''
-if parm == 'install':   mode = parm
-if parm == 'uninstall': mode = parm
-if parm == 'check':     mode = parm
-if mode == '':
-    print('')
-    print('Usage: setup.py install|uninstall|check')
-    print('')
-    sys.exit(0)
-
-if sys.argv[1].lower().strip() == 'install':
-    print('')
-    print('Perform installation ...')
-    print('Copy: ' + src_kernel_dir + ' => ' + os.path.join(site_packages_dir,'jkernel'))
-    try:
-        shutil.copytree(src_kernel_dir,os.path.join(site_packages_dir,'jkernel'))
-    except:
-        print_exception_info()
-    print('Copy: ' + src_kernel_definition_dir + ' => ' + os.path.join(jupyter_kernels_dir,'jkernel'))
-    try:
-        shutil.copytree(src_kernel_definition_dir,os.path.join(jupyter_kernels_dir,'jkernel'))
-    except:
-        print_exception_info()
-    print('Copy: ' + src_syntax_dir + ' => ' + os.path.join(notebook_syntax_dir,'J'))
-    try:
-        shutil.copytree(src_syntax_dir,os.path.join(notebook_syntax_dir,'J'))
-    except:
-        print_exception_info()
-    print('Done.')
-    print('')
-    print('###################################################################')
-    print('# PLEASE DO NOT FORGET TO SET THE FOLLOWING ENVIRONMENT VARIABLES #')
-    print('#                                                                 #')
-    print('# Linux or MacOS: ~/.bashrc or ~/.bash_profile                    #')
-    print('# --------------------------------------------                    #')
-    print('# export J_INSTALLATION_FOLDER="<J installation folder>"          #')
-    print('# export J_BIN_FOLDER="<J binaries folder>"                       #')
-    print('#                                                                 #')
-    print('# Windows:                                                        #')
-    print('# --------                                                        #')
-    print('# SET J_INSTALLATION_FOLDER=<J installation folder>               #')
-    print('# SET J_BIN_FOLDER=<J binaries folder>                            #')
-    print('# ... or via Control Panel ...                                    #')
-    print('###################################################################')
-    print('')
-    sys.exit(0)
-
-if sys.argv[1].lower().strip() == 'uninstall':
-    print('')
-    print('Perform un-installation ...')
-    print('Remove: ' + os.path.join(site_packages_dir,'jkernel'))
-    try:
-        shutil.rmtree(os.path.join(site_packages_dir,'jkernel'))
-    except:
-        print_exception_info()
-    print('Remove: ' + os.path.join(jupyter_kernels_dir,'jkernel'))
-    try:
-        shutil.rmtree(os.path.join(jupyter_kernels_dir,'jkernel'))
-    except:
-        print_exception_info()
-    print('Remove: ' + os.path.join(notebook_syntax_dir,'J'))
-    try:
-        shutil.rmtree(os.path.join(notebook_syntax_dir,'J'))
-    except:
-        print_exception_info()
-    print('Done.')
-    sys.exit(0)
-
-if sys.argv[1].lower().strip() == 'check':
-    print('')
-    print('Checking current installation ...')
-
-    all_ok = True
-
-    pth = os.path.join(site_packages_dir,'jkernel')
-    if os.access(pth,os.R_OK):
-        print(pth + ' => OK')
-    else:
-        print(pth + ' => MISSING')
-        all_ok = False
-    for f in src_kernel_files:
-        fn = os.path.basename(f)
-        df = os.path.join(pth,fn)
-        if os.access(df,os.R_OK):
-            print(df + ' => OK')
-        else:
-            print(df + ' => MISSING')
-            all_ok = False
-
-    pth = os.path.join(jupyter_kernels_dir,'jkernel')
-    if os.access(pth,os.R_OK):
-        print(pth + ' => OK')
-    else:
-        print(pth + ' => MISSING')
-        all_ok = False
-    for f in src_kernel_definition_files:
-        fn = os.path.basename(f)
-        df = os.path.join(pth,fn)
-        if os.access(df,os.R_OK):
-            print(df + ' => OK')
-        else:
-            print(df + ' => MISSING')
-            all_ok = False
-
-    pth = os.path.join(notebook_syntax_dir,'J')
-    if os.access(pth,os.R_OK):
-        print(pth + ' => OK')
-    else:
-        print(pth + ' => MISSING')
-        all_ok = False
-    for f in src_syntax_files:
-        fn = os.path.basename(f)
-        df = os.path.join(pth,fn)
-        if os.access(df,os.R_OK):
-            print(df + ' => OK')
-        else:
-            print(df + ' => MISSING')
-            all_ok = False
-
-    pth = os.getenv('J_INSTALLATION_FOLDER')
-    if pth is None: pth = ''
-    if pth != '':
-        print('Environment variable: J_INSTALLATION_FOLDER => OK')
-        if os.access(pth,os.R_OK):
-            print(pth + ' => OK')
-        else:
-            print(pth + ' => MISSING')
-            all_ok = False
-    else:
-        print('Environment variable: J_INSTALLATION_FOLDER => MISSING')
-        all_ok = False
-
-    pth = os.getenv('J_BIN_FOLDER')
-    if pth is None: pth = ''
-    if pth != '':
-        print('Environment variable: J_BIN_FOLDER => OK')
-        if os.access(pth,os.R_OK):
-            print(pth + ' => OK')
-        else:
-            print(pth + ' => MISSING')
-            all_ok = False
-    else:
-        print('Environment variable: J_BIN_FOLDER => MISSING')
-        all_ok = False
-
-    # Print check results
-    if all_ok == True:
-        print('ALL checks passed.')
-        print('Done.')
-    else:
-        print('ERROR: In one or more of the installation components !!!')
-
-    sys.exit(0)
-
-
-###############################################################################
-# EOF
-###############################################################################
